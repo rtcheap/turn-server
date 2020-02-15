31da5e69397c9d5b3011fb545f2fe331340d1c75
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-02-15 15:41:52 +0100

    Changed tracing operation names and logging

--- cmd/session_controller.go
+++ cmd/session_controller.go
@@ -12,50 +12,46 @@ import (
 )
 
 func (e *env) addSession(c *gin.Context) {
-	span, _ := opentracing.StartSpanFromContext(c.Request.Context(), "controller.addSession")
+	span, _ := opentracing.StartSpanFromContext(c.Request.Context(), "controller_add_session")
 	defer span.Finish()
 
 	var session dto.Session
 	err := c.BindJSON(&session)
 	if err != nil {
 		err = httputil.BadRequestError(fmt.Errorf("failed to parse request body. %w", err))
-		span.LogFields(tracelog.Bool("success", false), tracelog.Error(err))
+		span.LogFields(tracelog.Error(err))
 		c.Error(err)
 		return
 	}
 
 	err = e.userService.CreateKey(session)
 	if err != nil {
-		span.LogFields(tracelog.Bool("success", false), tracelog.Error(err))
+		span.LogFields(tracelog.Error(err))
 		c.Error(err)
 		return
 	}
 
-	span.LogFields(tracelog.Bool("success", true))
 	httputil.SendOK(c)
 }
 
 func (e *env) removeSession(c *gin.Context) {
-	span, _ := opentracing.StartSpanFromContext(c.Request.Context(), "controller.addSession")
+	span, _ := opentracing.StartSpanFromContext(c.Request.Context(), "controller_add_session")
 	defer span.Finish()
 
 	err := e.userService.Remove(c.Param("userID"))
 	if err != nil {
-		span.LogFields(tracelog.Bool("success", false), tracelog.Error(err))
+		span.LogFields(tracelog.Error(err))
 		c.Error(err)
 		return
 	}
 
-	span.LogFields(tracelog.Bool("success", true))
 	httputil.SendOK(c)
 }
 
 func (e *env) getSessionStatistics(c *gin.Context) {
-	span, _ := opentracing.StartSpanFromContext(c.Request.Context(), "controller.getSessionStatistics")
+	span, _ := opentracing.StartSpanFromContext(c.Request.Context(), "controller_get_session_statistics")
 	defer span.Finish()
 
 	stats := e.userService.GetStatistics()
-
-	span.LogFields(tracelog.Bool("success", true))
 	c.JSON(http.StatusOK, stats)
 }
